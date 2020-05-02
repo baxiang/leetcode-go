f710c3d0a304a2be428844432fbebc2fa924e04a
baxiang <baxiang@roobo.com>
2020-05-02 12:10:20 +0800

    数学计算

--- 17da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/main.go
+++ 17da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"math"
+	"fmt"
+)
+
+func printNumbers(n int) []int {
+    var res []int
+    var max int
+    for n>0{
+    	max =max*10+9
+    	n--
+	}
+    for i:=1;i<=max;i++{
+    	res = append(res,i)
+	}
+	return res
+}
+
+func printNumbers1(n int) []int {
+	var res []int
+	for i:=1;i<int(math.Pow10(n));i++{
+		res = append(res,i)
+	}
+	return res
+}
+
 func main() {
-	$END$
+	//fmt.Println(printNumbers1(1))
+	fmt.Println(printNumbers1(2))
+	//fmt.Println(printNumbers(3))
 }
